cf103d40796b048a2f016d0b3974fc81a3bb86f3
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-12-07 20:31:57 +0000

    Added getters and setters for the id, and dates

--- app/tasks/models.py
+++ app/tasks/models.py
@@ -8,12 +8,49 @@ class Task:
     """Task class"""
 
     def __init__(self, title: str, description: str, due_date, due_date_date) -> None:
+        """Initializes an instance of the Task class"""
         self.__id = uuid.uuid4()
         self.title = title
         self.description = description
         self.due_date = due_date
         self.due_date_date = due_date_date
         self.__created_at = datetime.now()
+        self.__updated_at = datetime.now()
     
+    @property
+    def id(self):
+        """Returns the id of the task"""
+        return self.__id
 
+    @property
+    def created_at(self):
+        """Returns the title of the task"""
+        return self.__created_at
+
+    @property
+    def updated_at(self):
+        """Returns the title of the task"""
+        return self.__updated_at
+
+    def to_dict(self):
+        """Returns a dictionary representation of the task"""
+        return {
+            'id': str(self.id),
+            'title': self.title,
+            'description': self.description,
+            'due_date': self.due_date,
+            'due_date_date': self.due_date_date,
+            'created_at': self.__created_at.strftime('%Y-%m-%dT%H:%M:%S.%f'),
+            'updated_at': self.__updated_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
+        }
+    
+    @created_at.setter
+    def created_at(self, value):
+        """Sets the created_at attribute to the current datetime"""
+        self.__created_at = value
+
+    @updated_at.setter
+    def updated_at(self, value):
+        """Sets the updated_at attribute to the current datetime"""
+        self.__updated_at = value
 
